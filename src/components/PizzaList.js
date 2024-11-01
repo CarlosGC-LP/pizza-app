7005f2a1e5ea2cc18302c7287b18fc452bbeab80
Carlos Gonzalez <carlos.custodi@hotmail.com>
2024-11-01 15:54:56 -0500

    Update PizzaList.js

--- src/components/PizzaList.js
+++ src/components/PizzaList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PizzaCard from './PizzaCard';
 
 const pizzas = [
-  { name: 'Pizza Vegetariana', price: 40, rating: 4.2 },
+  { name: 'Pizza Vegetariana', price: 40, rating: 4.2, imageUrl: 'https://cdn.shopify.com/s/files/1/0191/9978/files/Pizza-Veggie-Supreme-blog.jpg?v=1652775259'},
   { name: 'Pizza con Pepperoni', price: 50, rating: 5.0 },
   { name: 'Pizza Suprema', price: 50, rating: 4.0 },
   { name: 'Pizza Margarita', price: 80, rating: 3.4 },
