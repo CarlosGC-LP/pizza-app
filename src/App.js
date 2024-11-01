b2c1fc8833da82153bd83bdbf1ac7ffe41ec0c4a
Carlos Gonzalez <carlos.custodi@hotmail.com>
2024-11-01 15:30:43 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import HomeScreen from "./components/HomeScreen";
-import Checkout from "./components/Checkout";
-import Payment from "./components/Payment";
-import Login from "./components/Login";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import PizzaList from './components/PizzaList';
+import Checkout from './components/Checkout';
+import Payment from './components/Payment';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<HomeScreen />} />
+        <Route path="/" element={<PizzaList />} />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/payment" element={<Payment />} />
-        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
