3dd72790a0bb420e3561303cac2e5f741a664f50
Carlos Gonzalez <carlos.custodi@hotmail.com>
2024-11-01 15:15:46 -0500

    Update PizzaList.js

---
+++ src/components/PizzaList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PizzaCard from './PizzaCard';
+
+const pizzas = [
+  { name: 'Pizza Vegetariana', price: 40, rating: 4.2 },
+  { name: 'Pizza con Pepperoni', price: 50, rating: 5.0 },
+  { name: 'Pizza Suprema', price: 50, rating: 4.0 },
+  { name: 'Pizza Margarita', price: 80, rating: 3.4 },
+];
+
+const PizzaList = () => {
+  return (
+    <div className="grid grid-cols-2 gap-4 p-4">
+      {pizzas.map((pizza, index) => (
+        <PizzaCard key={index} pizza={pizza} />
+      ))}
+    </div>
+  );
+};
+
+export default PizzaList;
